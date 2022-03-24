6d16883a3cb0dcf856d33b8e6ed35787328bf815
Alexander Chervoncev <chervoncev2015@gmail.com>
2022-03-24 19:37:40 +0300

    Home Work 1.9

---
+++ HomeWork9-1/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  HomeWork9-1
+//
+//  Created by Alexander Chervoncev on 22.03.22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+   @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
